86583bc4159ece2e7d09af797a1cb71f8ce1d963
mir-hussain <mir.hm110@gmail.com>
2024-07-20 16:24:04 +0600

    refactor: two number sum solution with two for loop

--- 01-Two-Number-Sum/twoNumberSum.js
+++ 01-Two-Number-Sum/twoNumberSum.js
@@ -5,23 +5,20 @@
 const numberArray = [3, 5, -4, 8, 11, 1, -1, 6, 4];
 const targetSum = 10;
 
-// solution 1
+// solution 1 - two loop O(n^2) Time | O(1) space
 
 const twoNumberSum1 = (arr, target) => {
-  const pair = [];
-
   for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (i === j) {
         continue;
       }
       if (arr[i] + arr[j] === target) {
-        pair.push([arr[i], arr[j]]);
+        return [arr[i], arr[j]];
       }
     }
   }
-
-  return pair;
+  return [];
 };
 
 const pairs = twoNumberSum1(numberArray, targetSum);
